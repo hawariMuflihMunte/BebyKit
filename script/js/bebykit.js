3b9615ccc4e82d74958a56d6760b594ed1fa1118
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2022-02-11 19:52:27 +0700

    Add files via upload

--- script/js/bebykit.js
+++ script/js/bebykit.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
     var windowSize = $(window).width();
 
-    if ($('nav').parent().hasClass('navbar-fixed')) {
-    	$('html').css('padding-top','50.66666px');
-    }
+    // if ($('nav').parent().hasClass('navbar-fixed')) {
+    // 	$('html').css('padding-top','50.66666px');
+    // }
 
     $('.navbar-menu > a').attr('href','javascript:void(0);');
     $('.navbar-menu > a').click(function() {
